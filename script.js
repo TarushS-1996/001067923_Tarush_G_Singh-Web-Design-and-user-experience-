d3ad598646c54811e6b94c9a3d76f630ba574238
TarushS-1996 <tarush.singh29@gmail.com>
2023-02-02 00:49:58 -0500

    Changes to certain scripts

--- script.js
+++ script.js
@@ -1,143 +1,196 @@
-function Title(t1) {
-  this.mytitle = t1;
+//Title constructor function that creates a Title object
+function Title(t1) 
+{ this.mytitle = t1;
 }
 
-Title.prototype.getName = function () {
-  return (this.mytitle);
+Title.prototype.getName = function () 
+{ 
+return (this.mytitle);
 }
 
 var socialMedia = {
-  facebook: 'http://facebook.com',
+  facebook : 'http://facebook.com',
   twitter: 'http://twitter.com',
   flickr: 'http://flickr.com',
   youtube: 'http://youtube.com'
 };
 
-var x = 1;
-function myCreateFunction() {
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(-1);
-  row.class="note";
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
-  var cell6 = row.insertCell(5);
-  var cell7 = row.insertCell(6);
-  var cell8 = row.insertCell(7);
-  var cell9 = row.insertCell(8);
-
-  cell1.innerHTML = "<input type='checkbox' onclick='yellow(this)' id='remember'> <br> <br><img src='down.png' style='width:25px' class='exit' id='myDiv'+ x onclick='myFunction(this)'/>";
-
-  cell2.innerHTML = "Student "+x;
-  cell3.innerHTML = "Teacher "+x;
-  cell4.innerHTML = "Approved";
-  cell5.innerHTML = "Fall";
-  cell6.innerHTML = "TA";
-  cell7.innerHTML = "2356";
-  cell8.innerHTML = "100%";
-  cell9.innerHTML="<button onclick='del(this)'>Delete</button>";
-  cell9.id="sid"+x;
-  document.getElementById("sid"+x).style.display="none";
-  var rowx = table.insertRow(-1);
-  rowx.id = "myDiv" + x;
-  var cell10 = rowx.insertCell(0);
-  cell10.id="cool"+x
-  document.getElementById("cool"+x).colSpan = "8"; 
-  cell10.innerHTML =  "Advisor: <br><br> Award Details <br> Summer 1-2014(TA) <br>Budget Number:<br> Tuition Number:<br>Comments:<br><br><br>Award Status:<br><br><br>";
-
-  x = x + 1;
-  alert("New Record added");
-  //console.log(x);
-
-}
-function btnedit()
-{
-  alert("Sucesfully edited");
-
-}
-
-
-function del(r) {
-  var i = r.parentNode.parentNode.rowIndex;
-  document.getElementById("myTable").deleteRow(i);
-  document.getElementById("myTable").deleteRow(i++);
-  console.log(i);
-  alert("that Record deleted successfully.");
-  x--;
-}
-
-
-
-
-
-function yellow(el){
-  if (el.checked){ 
-  el.parentElement.parentElement.classList.toggle("selected");
-  console.log(el);
-  var sb = document.getElementById("sb");
-  console.log(sb);
-  sb.disabled = false;
-  sb.className = 'enable';
-
-  var sb = document.getElementById("ss");
-  sb.hidden=false;
-  deledit();  
-  //alert("checked");
-  }
-  else{
-    var sb = document.getElementById("sb");
-    sb.disabled = true;
-    sb.className="";
-
-    var sb = document.getElementById("ss");
-    ss.className = "";
-    sb.hidden=true;
-    deledit1();
-    el.parentElement.parentElement.classList.toggle("selected");
-    
-  }
-}
-
-
-
-  
-
-function myFunction(z){
-  console.log(z);
-  
-  var j = z.parentNode.parentNode.rowIndex;
-  console.log(j);
-  j++;
-  var s = document.getElementsByTagName("tr")[j];
-
-  if (s.style.display === "none") {
-    s.style.display = "table-row";
-  } else {
-    s.style.display = "none";
-  }
-}
-  
-
-function deledit(yy){
-  x--;
-  var yy = document.getElementById("sid"+x);
-  console.log(yy);
-  yy.style.display="table-cell";
-  x++;
-}
-
-function deledit1(yy){
-
-  x--;
-  var yy = document.getElementById("sid"+x);
-  console.log(yy);
-  yy.style.display="none";
-  x++;
-
-}
-
-
-
 var t = new Title("CONNECT WITH ME!");
+
+//script
+
+function getRandom(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+let inpchk = document.querySelectorAll("input[type=checkbox]");
+
+function handleChkboxTgle(checkbox) {
+  console.log(checkbox.checked);
+
+  let Row = checkbox.parentElement.parentElement;
+
+  console.log(Row);
+
+  let delColmn = Row.querySelector(":nth-child(9)");
+  removeAllChldNode(delColmn);
+
+  let editColmn = Row.querySelector(":nth-child(10)");
+  removeAllChldNode(editColmn);
+
+  Row.bgColor = '';
+
+  if (checkbox.checked) {
+    Row.bgColor = 'yellow';
+    delColmn.appendChild(newDelBtn(Row));
+    editColmn.appendChild(newEditBtn());
+
+  }
+
+  submitBtnandColVisibilitytoggle();
+}
+
+function removeAllChldNode(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+function defAlert(text) {
+  setTimeout(() => {
+    alert(text);
+  }, 150);
+}
+
+function newDelBtn(tr) {
+  let btn = document.createElement('input');
+
+  btn.type = 'button';
+
+  btn.value = 'Delete';
+
+  btn.addEventListener('click', () => {
+    tr.nextElementSibling.remove();
+    tr.remove();
+    submitBtnandColVisibilitytoggle();
+    defAlert('record deleted successfully');
+  })
+
+  return btn;
+}
+
+function newEditBtn() {
+  let btn = document.createElement('input');
+
+  btn.type = 'button';
+
+  btn.value = "Edit";
+  btn.addEventListener('click', () => {
+    const userInput = prompt("Edit the details")
+    console.log(userInput);
+  })
+
+  return btn;
+}
+
+function submitBtnandColVisibilitytoggle() {
+  let chkboxes = document.querySelectorAll("input[type=checkbox]");
+
+  let checked = false;
+
+  chkboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      checked = true;
+      }
+    })
+
+    document.querySelector("button#button").disabled = !checked;
+
+    const deleteAndEditRows = document.querySelectorAll("#myTable td:nth-child(9),#myTable th:nth-child(9),#myTable td:nth-child(10),#myTable th:nth-child(10)");
+
+    deleteAndEditRows.forEach(cell => {
+      cell.classList.remove("display-cell");
+      if (checked) {
+        cell.classList.add("display-cell");
+      }
+  })
+
+  console.log(`button disability is now ${!checked}`);
+}
+
+function tglDropDwn(img) {
+  const drpdwn = img.parentElement.parentElement.nextElementSibling;
+  console.log(drpdwn);
+
+  if (drpdwn.classList.contains("dropDownTextArea")) {
+    drpdwn.classList.remove("dropDownTextArea");
+  }
+  else {
+    drpdwn.classList.add("dropDownTextArea");
+  }
+}
+
+function InputFunctionality(input) {
+  input.addEventListener('change', () => {
+    handleChkboxTgle(input);
+  })
+
+  const img = input.nextElementSibling.nextElementSibling.nextElementSibling;
+
+  img.addEventListener('click', () => {
+    tglDropDwn(img);
+  })
+
+  handleChkboxTgle(input);
+}
+
+let cln = document.querySelectorAll("#myTable tbody tr");
+let clnRow = cln[1].cloneNode(true);
+let clnDetails = cln[2].cloneNode(true);
+
+inpchk.forEach(input => {
+  InputFunctionality(input);
+})
+
+function addNewStudent() {
+  console.log(`adding new student`);
+
+  let tbody = document.querySelector("#myTable tbody");
+
+  newRow = clnRow.cloneNode(true);
+  newDetails = clnDetails.cloneNode(true);
+
+  const count = document.querySelectorAll("input[type=checkbox]").length + 1;
+
+  const rows = newRow.querySelectorAll("td");
+
+  rows[1].innerHTML = `Student ${count}`;
+  rows[2].innerHTML = `Teacher ${count}`;
+  rows[6].innerHTML = getRandom(count * 10000, count * 10000 + 9999);
+
+  let checkbox = newRow.querySelector("input[type=checkbox]");
+  InputFunctionality(checkbox);
+
+  console.log(newRow);
+  console.log(newDetails);
+
+  tbody.appendChild(newRow);
+  tbody.appendChild(newDetails);
+
+  defAlert("New student added successfully");
+}
+
+let addNewButton = document.querySelector("button#add");
+
+addNewButton.addEventListener('click', () => {
+  try {
+    addNewStudent();
+    submitBtnandColVisibilitytoggle();
+  } catch (error) {
+      alert(`Unable to add student: ${error}`);
+    }
+})
+
