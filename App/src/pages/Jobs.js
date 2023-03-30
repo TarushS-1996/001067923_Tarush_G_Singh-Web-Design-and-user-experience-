46bdf0288f635f4381a924887a53dca81669633f
TarushS-1996 <tarush.singh29@gmail.com>
2023-03-30 15:12:55 -0400

    changes to the pages sheet

--- App/src/pages/Jobs.js
+++ App/src/pages/Jobs.js
@@ -10,7 +10,7 @@ function Jobs(){
         <Card>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Job 1</Card.Title>
             <Card.Text>
               This is a wider card with supporting text below as a natural lead-in
               to additional content. This content is a little bit longer.
@@ -23,7 +23,7 @@ function Jobs(){
         <Card>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Job 2</Card.Title>
             <Card.Text>
               This card has supporting text below as a natural lead-in to
               additional content.{' '}
@@ -36,7 +36,7 @@ function Jobs(){
         <Card>
           <Card.Img variant="top" src="holder.js/100px160" />
           <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Job 3</Card.Title>
             <Card.Text>
               This is a wider card with supporting text below as a natural lead-in
               to additional content. This card has even longer content than the
